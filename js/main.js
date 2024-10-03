0c3ade4330adbb9a062ccba8eaca2c944b367788
Deven Kohli <devenk120@gmail.com>
2024-10-02 21:20:07 -0600

    in class 3
    pushed lab 3

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
 // Enter your code below.
+
+console.log("is this working?");
+
+document.addEventListener('DOMContentLoaded', function() {
+const showbutton = document.querySelector('#show-resources');
+const resourcesdiv = document.querySelector('.javascript-resources');
+
+showbutton.addEventListener('click', function () {
+    resourcesdiv.classList.remove('d-none');
+});
+
+resourcesdiv.addEventListener('mouseover', function (event) {
+    if (event.target.tagName === 'A') {
+        event.target.classList.add('fw-bold');
+    }
+});
+
+resourcesdiv.addEventListener('mouseout', function (event) {
+    if (event.target.tagName === 'A') {
+        event.target.classList.remove('fw-bold');
+    }
+});
+
+resourcesdiv.addEventListener('click', function (event) {
+    if (event.target.tagName === 'A') {
+        event.target.classList.add('fst-italic');
+    }
+});
+
+});
